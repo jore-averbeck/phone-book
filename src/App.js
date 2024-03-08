bcd89a1a98ccfe066bfa4f85c3c7c2a7572ac833
Jore <noa.averbeck@gmail.com>
2024-03-08 09:20:50 +0100

    connect data to frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React from "react";
 import "./styles.css";
+import { useQuery, gql } from "@apollo/client"
+import { Typography, Container} from "@mui/material";
+
+const getAllNumbers = gql`
+  query GetPhoneNumbers {
+    phoneNumbers {
+      id,
+      name,
+      phone
+    }
+  }
+`;
+
 
 export default function App() {
- 
+  const { loading, error, data } = useQuery(getAllNumbers);
+  
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  if(!data) return <p>No data found</p>
+
 
   return (
-   <h1>Hello World!</h1>
+    <Container>
+      <Typography variant="h1" color="primary" align="center">Phone Book</Typography>
+      {data.phoneNumbers.map(phone => (
+        <Typography key={phone.id}>{phone.name} - {phone.phone}</Typography>
+      ))}
+    </Container>
   );
 }
