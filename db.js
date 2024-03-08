d7d4b5534c2239ef3e3d7b111ac321b39feedfb1
Jore <noa.averbeck@gmail.com>
2024-03-08 09:11:28 +0100

    add data

---
+++ db.js
@@ -0,0 +1,605 @@
+export const phoneNumbers = [
+    {
+      id: "1",
+      name: "Harper-Rose Lennon",
+      phone: "0171/24984023"
+    },
+    {
+      id: "2",
+      name: "Lloyd Mitchell",
+      phone: "0171/14294053"
+    },
+    {
+      id: "3",
+      name: "Aminah Sheldon",
+      phone: "0176/33581541"
+    },
+    {
+      id: "4",
+      name: "Skylah Gibbons",
+      phone: "0176/30053109"
+    },
+    {
+      id: "5",
+      name: "Owen Wilkes",
+      phone: "0176/52364266"
+    },
+    {
+      id: "6",
+      name: "Kajol Carty",
+      phone: "0176/23200162"
+    },
+    {
+      id: "7",
+      name: "Mohammod Finch",
+      phone: "0171/30608407"
+    },
+    {
+      id: "8",
+      name: "Khaleesi Randolph",
+      phone: "0171/75813522"
+    },
+    {
+      id: "9",
+      name: "Bryan Rankin",
+      phone: "0171/38068474"
+    },
+    {
+      id: "10",
+      name: "Luke Mason",
+      phone: "0176/47664269"
+    },
+    {
+      id: "11",
+      name: "Nathan Barrett",
+      phone: "0170/80707589"
+    },
+    {
+      id: "12",
+      name: "Benjamin Turner",
+      phone: "0176/56087619"
+    },
+    {
+      id: "13",
+      name: "Cameron Fletcher",
+      phone: "0176/20338076"
+    },
+    {
+      id: "14",
+      name: "Noah Russell",
+      phone: "0171/79150360"
+    },
+    {
+      id: "15",
+      name: "Kevin Lowe",
+      phone: "0170/44920493"
+    },
+    {
+      id: "16",
+      name: "Conrad Rice",
+      phone: "0176/50131376"
+    },
+    {
+      id: "17",
+      name: "Joseph Quinn",
+      phone: "0170/91129315"
+    },
+    {
+      id: "18",
+      name: "Nicolas Davis",
+      phone: "0171/92954564"
+    },
+    {
+      id: "19",
+      name: "Luke Mason",
+      phone: "0170/26939883"
+    },
+    {
+      id: "20",
+      name: "Rylee Ramirez",
+      phone: "0170/20538429"
+    },
+    {
+      id: "21",
+      name: "Chardonnay Braun",
+      phone: "0176/10682286"
+    },
+    {
+      id: "22",
+      name: "Norma Bull",
+      phone: "0170/27755581"
+    },
+    {
+      id: "23",
+      name: "Mikayla Davidson",
+      phone: "0171/62497885"
+    },
+    {
+      id: "24",
+      name: "Aneurin Anthony",
+      phone: "0171/30487082"
+    },
+    {
+      id: "25",
+      name: "Iwan Delacruz",
+      phone: "0171/21165707"
+    },
+    {
+      id: "26",
+      name: "Maximillian Wilkes",
+      phone: "0170/72172182"
+    },
+    {
+      id: "27",
+      name: "Junaid Fox",
+      phone: "0170/68530272"
+    },
+    {
+      id: "28",
+      name: "Sophia Hutton",
+      phone: "0170/93035398"
+    },
+    {
+      id: "29",
+      name: "Leon Palmer",
+      phone: "0170/84722554"
+    },
+    {
+      id: "30",
+      name: "Alastair Beil",
+      phone: "0176/95916812"
+    },
+    {
+      id: "31",
+      name: "Sasha Fernandez",
+      phone: "0171/49954922"
+    },
+    {
+      id: "32",
+      name: "Clarence Webb",
+      phone: "0176/86067147"
+    },
+    {
+      id: "33",
+      name: "Jonny Donaldson",
+      phone: "0171/14925659"
+    },
+    {
+      id: "34",
+      name: "Lacey Summers",
+      phone: "0170/21951718"
+    },
+    {
+      id: "35",
+      name: "Marley Riggs",
+      phone: "0176/63707318"
+    },
+    {
+      id: "36",
+      name: "Samirah Paine",
+      phone: "0171/19510136"
+    },
+    {
+      id: "37",
+      name: "Lilly-Rose Kearns",
+      phone: "0171/42915424"
+    },
+    {
+      id: "38",
+      name: "Kelsi Kerr",
+      phone: "0171/99514375"
+    },
+    {
+      id: "39",
+      name: "Isabella Holman",
+      phone: "0171/90736723"
+    },
+    {
+      id: "40",
+      name: "Emily-Jane Truong",
+      phone: "0170/66194924"
+    },
+    {
+      id: "41",
+      name: "Gabrielius Thomas",
+      phone: "0170/37168186"
+    },
+    {
+      id: "42",
+      name: "Darla Coulson",
+      phone: "0176/30601912"
+    },
+    {
+      id: "43",
+      name: "Nicole Driscoll",
+      phone: "0171/53120521"
+    },
+    {
+      id: "44",
+      name: "Roxie Sharma",
+      phone: "0176/73490743"
+    },
+    {
+      id: "45",
+      name: "Kajus Hagan",
+      phone: "0171/34200706"
+    },
+    {
+      id: "46",
+      name: "Elaine Avery",
+      phone: "0176/90255306"
+    },
+    {
+      id: "47",
+      name: "Deen Huynh",
+      phone: "0171/16624752"
+    },
+    {
+      id: "48",
+      name: "Shahzaib Crowther",
+      phone: "0171/55790473"
+    },
+    {
+      id: "49",
+      name: "Aisling Travis",
+      phone: "0171/75344741"
+    },
+    {
+      id: "50",
+      name: "Isabel Hodgson",
+      phone: "0170/32913021"
+    },
+    {
+      id: "51",
+      name: "Aliesha Mackie",
+      phone: "0171/76255792"
+    },
+    {
+      id: "52",
+      name: "Edie Kumar",
+      phone: "0171/59841108"
+    },
+    {
+      id: "53",
+      name: "Filip Swift",
+      phone: "0170/82494207"
+    },
+    {
+      id: "54",
+      name: "Blythe Davey",
+      phone: "0170/21471752"
+    },
+    {
+      id: "55",
+      name: "Jeevan Huerta",
+      phone: "0176/49859941"
+    },
+    {
+      id: "56",
+      name: "Sebastian Philip",
+      phone: "0171/98223042"
+    },
+    {
+      id: "57",
+      name: "Ebonie Bush",
+      phone: "0171/88747394"
+    },
+    {
+      id: "58",
+      name: "Gary Finney",
+      phone: "0171/19250031"
+    },
+    {
+      id: "59",
+      name: "Lois Frederick",
+      phone: "0170/41221161"
+    },
+    {
+      id: "60",
+      name: "Jaydn Powers",
+      phone: "0171/30916829"
+    },
+    {
+      id: "61",
+      name: "Eamonn Hammond",
+      phone: "0171/34696615"
+    },
+    {
+      id: "62",
+      name: "Marissa Bradley",
+      phone: "0171/95549226"
+    },
+    {
+      id: "63",
+      name: "Umaiza Ryan",
+      phone: "0176/40472052"
+    },
+    {
+      id: "64",
+      name: "Safiyyah Cisneros",
+      phone: "0171/37145825"
+    },
+    {
+      id: "65",
+      name: "Josef Singh",
+      phone: "0170/26186015"
+    },
+    {
+      id: "66",
+      name: "Jorja Combs",
+      phone: "0176/30618912"
+    },
+    {
+      id: "67",
+      name: "Che Power",
+      phone: "0171/55764063"
+    },
+    {
+      id: "68",
+      name: "Cruz Klein",
+      phone: "0176/54877713"
+    },
+    {
+      id: "69",
+      name: "Kyron Church",
+      phone: "0171/32733229"
+    },
+    {
+      id: "70",
+      name: "Kairon Goodman",
+      phone: "0170/89335430"
+    },
+    {
+      id: "71",
+      name: "Diya Cervantes",
+      phone: "0171/39162781"
+    },
+    {
+      id: "72",
+      name: "Mairead Wiggins",
+      phone: "0171/86690471"
+    },
+    {
+      id: "73",
+      name: "Talia Weaver",
+      phone: "0170/65486309"
+    },
+    {
+      id: "74",
+      name: "Marguerite Wade",
+      phone: "0171/93437151"
+    },
+    {
+      id: "75",
+      name: "Lily-May Alvarado",
+      phone: "0171/4527294"
+    },
+    {
+      id: "76",
+      name: "Branden Bevan",
+      phone: "0171/80293987"
+    },
+    {
+      id: "77",
+      name: "Raveena Silva",
+      phone: "0176/23658914"
+    },
+    {
+      id: "78",
+      name: "Serenity East",
+      phone: "0171/71290673"
+    },
+    {
+      id: "79",
+      name: "Laith Lowry",
+      phone: "0176/2477659"
+    },
+    {
+      id: "80",
+      name: "Bea Kirkland",
+      phone: "0171/16180681"
+    },
+    {
+      id: "81",
+      name: "Korban Whitaker",
+      phone: "0170/20573792"
+    },
+    {
+      id: "82",
+      name: "Elyse Carpenter",
+      phone: "0170/5937630"
+    },
+    {
+      id: "83",
+      name: "Nicola Rowe",
+      phone: "0171/41248197"
+    },
+    {
+      id: "84",
+      name: "Leslie Vickers",
+      phone: "0171/49114589"
+    },
+    {
+      id: "85",
+      name: "Anna Horner",
+      phone: "0171/34508922"
+    },
+    {
+      id: "86",
+      name: "Ranveer Wilde",
+      phone: "0171/66055906"
+    },
+    {
+      id: "87",
+      name: "Enoch French",
+      phone: "0171/24111872"
+    },
+    {
+      id: "88",
+      name: "Eilidh Pineda",
+      phone: "0176/81354607"
+    },
+    {
+      id: "89",
+      name: "Amy Piper",
+      phone: "0170/95114407"
+    },
+    {
+      id: "90",
+      name: "Dawson Roberts",
+      phone: "0176/75525761"
+    },
+    {
+      id: "91",
+      name: "Hallam Fritz",
+      phone: "0176/33044514"
+    },
+    {
+      id: "92",
+      name: "Jorja Appleton",
+      phone: "0171/39720026"
+    },
+    {
+      id: "93",
+      name: "Ray Bloom",
+      phone: "0170/31952070"
+    },
+    {
+      id: "94",
+      name: "Evie-Grace Sharpe",
+      phone: "0170/21770212"
+    },
+    {
+      id: "95",
+      name: "Marcus Betts",
+      phone: "0170/61103936"
+    },
+    {
+      id: "96",
+      name: "Ayaan Oliver",
+      phone: "0171/55343758"
+    },
+    {
+      id: "97",
+      name: "Everly Stone",
+      phone: "0171/24881545"
+    },
+    {
+      id: "98",
+      name: "Hassan Mccann",
+      phone: "0176/19325469"
+    },
+    {
+      id: "99",
+      name: "Amirah Burt",
+      phone: "0170/25305255"
+    },
+    {
+      id: "100",
+      name: "Isra Porter",
+      phone: "0171/27539910"
+    },
+    {
+      id: "101",
+      name: "Ari Hernandez",
+      phone: "0170/56617474"
+    },
+    {
+      id: "102",
+      name: "Tolga Bass",
+      phone: "0171/25845817"
+    },
+    {
+      id: "103",
+      name: "Elouise Kramer",
+      phone: "0171/90059830"
+    },
+    {
+      id: "104",
+      name: "Aniya Esparza",
+      phone: "0170/54939734"
+    },
+    {
+      id: "105",
+      name: "Andre Morley",
+      phone: "0176/10799355"
+    },
+    {
+      id: "106",
+      name: "Kane Rodriguez",
+      phone: "0176/91440224"
+    },
+    {
+      id: "107",
+      name: "Shelby Warner",
+      phone: "0170/4629760"
+    },
+    {
+      id: "108",
+      name: "Jareth Hahn",
+      phone: "0171/72930791"
+    },
+    {
+      id: "109",
+      name: "Lillie-May Cantrell",
+      phone: "0170/39890163"
+    },
+    {
+      id: "110",
+      name: "Abbigail Robles",
+      phone: "0171/81492735"
+    },
+    {
+      id: "111",
+      name: "Iain Bauer",
+      phone: "0171/61255182"
+    },
+    {
+      id: "112",
+      name: "Reagan Orr",
+      phone: "0170/49072393"
+    },
+    {
+      id: "113",
+      name: "Jadon Ashley",
+      phone: "0176/26299708"
+    },
+    {
+      id: "114",
+      name: "Nichole Mason",
+      phone: "0170/85229908"
+    },
+    {
+      id: "115",
+      name: "Vincent Philip",
+      phone: "0176/28484061"
+    },
+    {
+      id: "116",
+      name: "Sannah Woodward",
+      phone: "0171/61047795"
+    },
+    {
+      id: "117",
+      name: "Gianni Mcdermott",
+      phone: "0176/7067132"
+    },
+    {
+      id: "118",
+      name: "Kelan Case",
+      phone: "0176/13401728"
+    },
+    {
+      id: "119",
+      name: "May Gould",
+      phone: "0171/34754720"
+    },
+    {
+      id: "120",
+      name: "Kiyan Ferreira",
+      phone: "0171/33603053"
+    }
+  ];
+  
+
+  
