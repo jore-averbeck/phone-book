951db125b88d3370e28ec3acf9b07445efeb83ad
Jore <noa.averbeck@gmail.com>
2024-03-11 16:07:40 +0100

    add Icon and update the input field

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,17 +1,27 @@
-import { Typography, Input, Button } from "@mui/material";
+import { Typography, Input, InputAdornment, IconButton } from "@mui/material";
+import { Clear } from '@mui/icons-material';
 
-export default function SearchInput({handleChange, searchTerm, phoneList, onReset}){
-
-    return(
-        <> 
-        <Input onChange={handleChange}  name="input" value={searchTerm} placeholder="Search"/>
-        <Button onClick={onReset} variant="outlined">x</Button>
+export default function SearchInput({ handleChange, searchTerm, phoneList, onReset }) {
+  return (
+    <>
+      <Input 
+        onChange={handleChange} 
+        name="input" 
+        value={searchTerm} 
+        placeholder="Search"
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton onClick={onReset} size="small">
+              <Clear />
+            </IconButton>
+          </InputAdornment>
+        }
+      />
       {phoneList.length > 0 ? (
         phoneList.map(phone => (
           <Typography key={phone.id}>{phone.name} - {phone.phone}</Typography>
         ))
       ) : <Typography>No phone numbers found</Typography>}
-        </>
-       
-    )
+    </>
+  )
 }
